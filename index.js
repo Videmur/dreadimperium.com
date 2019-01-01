f4dff12f21600ce59203a1abb61594055e664a12
Shmueli Englard <shmueli.yosef@englard.net>
2019-01-01 14:02:59 -0500

    Stars are now regenerated on resize

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 (function () {
     "use strict";
-    // Sets the number of stars we wish to display
-    var numStars = 100;
-    // For every star we want to display
-    for (var i = 0; i < numStars; i++) {
-        var star = document.createElement("div");
-        star.className = "star";
-        var xy = getRandomPosition();
-        star.style.top = xy[0] + 'px';
-        star.style.left = xy[1] + 'px';
-        document.body.append(star);
+    var starsDiv = document.createElement("div");
+    starsDiv.id = "stars";
+    document.body.append(starsDiv);
+    function generateStars() {
+        // Sets the number of stars we wish to display
+        var numStars = 100 * window.innerWidth / window.innerHeight;
+        starsDiv.innerHTML = "";
+        // For every star we want to display
+        for (var i = 0; i < numStars; i++) {
+            var star = document.createElement("div");
+            star.className = "star";
+            var xy = getRandomPosition();
+            star.style.top = xy[0] + 'px';
+            star.style.left = xy[1] + 'px';
+            starsDiv.append(star);
+        }
     }
     // Gets random x, y values based on the size of the container
     function getRandomPosition() {
@@ -19,5 +25,7 @@
         var randomY = Math.floor(Math.random() * y);
         return [randomX, randomY];
     }
+    window.addEventListener("resize", generateStars);
+    generateStars();
 })();
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,25 +1,35 @@
 (function () {
     "use strict";
 
-    // Sets the number of stars we wish to display
-    const numStars = 100;
+    const starsDiv = document.createElement("div");
+    starsDiv.id = "stars";
+    document.body.append(starsDiv);
 
-    // For every star we want to display
-    for (let i = 0; i < numStars; i++) {
-        let star = document.createElement("div");
-        star.className = "star";
-        var xy = getRandomPosition();
-        star.style.top = xy[0] + 'px';
-        star.style.left = xy[1] + 'px';
-        document.body.append(star);
+    function generateStars(): void {
+        // Sets the number of stars we wish to display
+        const numStars = 100 * window.innerWidth / window.innerHeight;
+        starsDiv.innerHTML = "";
+
+        // For every star we want to display
+        for (let i = 0; i < numStars; i++) {
+            let star = document.createElement("div");
+            star.className = "star";
+            var xy = getRandomPosition();
+            star.style.top = xy[0] + 'px';
+            star.style.left = xy[1] + 'px';
+            starsDiv.append(star);
+        }
     }
 
     // Gets random x, y values based on the size of the container
-    function getRandomPosition() {
+    function getRandomPosition(): [number, number] {
         var y = window.innerWidth;
         var x = window.innerHeight;
         var randomX = Math.floor(Math.random() * x);
         var randomY = Math.floor(Math.random() * y);
         return [randomX, randomY];
     }
+
+    window.addEventListener("resize", generateStars);
+    generateStars();
 })();
